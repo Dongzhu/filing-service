d239c87209d4d74f338a216b8cfd1a590b8b0dc5
Dongzhu <tony@idongzhu.com>
2022-12-28 12:36:40 +0800

    Built the project and modules structure and directories. built passed. 初步构建项目和模块结构和目录结构，项目整体build通过。

---
+++ src/main/java/com/example/filingservice/FilingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.filingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilingServiceApplication.class, args);
+    }
+
+}
